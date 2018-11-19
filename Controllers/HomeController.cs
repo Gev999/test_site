c11607ff63958b81de4fdcae4491bc8145ce66ab
Gev Sargsyan <gevorg.sargsyan999@mail.ru>
2018-11-19 21:43:51 +0400

    ASP.NET Core + MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstSite.Models;
+
+namespace FirstSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Account account)
+        {
+            //if (ModelState.IsValid) return View();
+            if (ModelState["Password"].ValidationState !=
+                Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid
+               && ModelState["Email"].ValidationState !=
+               Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid) return View();
+
+            return View("Index", account);
+        }
+
+        [HttpPost]
+        public IActionResult Register()
+        {
+            if (ModelState.IsValid) return View();
+            return View("Index");
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstSite.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required (ErrorMessage="Please enter email")]
+        [EmailAddress (ErrorMessage = "Not correct email")]
+        public string Email { get; set; }
+
+        [Required (ErrorMessage ="Please enter password")]  
+        public string Password { get; set; }
+
+        public int DayOfBirth { get; set; }
+        public string MonthOfBirth { get; set; }
+        public string YearOfBirth { get; set; }
+        public string Sex { get; set; }
+    }
+}
